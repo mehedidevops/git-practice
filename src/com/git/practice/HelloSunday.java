0c849fa3979172fefeb34d5d11b4b0847af595fd
Mehedi Aziz <mehediqa@yahoo.com>
2022-08-14 08:31:43 -0400

    this my 1st commit

---
+++ src/com/git/practice/HelloSunday.java
@@ -0,0 +1,18 @@
+package com.git.practice;
+
+public class HelloSunday {
+
+    public static void main(String[] args) {
+
+        System.out.println(helloJavaStudents());
+
+    }
+
+
+    public static String helloJavaStudents(){
+        return "How are you!, How was your course learning";
+    }
+
+
+
+}
